dc1c986cdb52ac909668ed248200794d02ba5cc4
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-10 18:24:11 +0600

    [Feature] Scroll View Reader

---
+++ SwiftfulThinkingContinuedLearning/ScrollViewReaderBootcamp.swift
@@ -0,0 +1,57 @@
+//
+//  ScrollViewReaderBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by jyotirmoy_halder on 10/8/25.
+//
+
+import SwiftUI
+
+struct ScrollViewReaderBootcamp: View {
+    
+    @State var scrollToIndex: Int = 0
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Enter a # here...", text: $textFieldText)
+                .frame(height: 55)
+                .border(Color.gray)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("SCROLL NOW") {
+                withAnimation(.spring()) {
+                    if let index = Int(textFieldText) {
+                        scrollToIndex = index
+                    }
+                }
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ForEach(0..<50) { index in
+                        Text("This is item #\(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { oldValue, newValue in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(newValue, anchor: .top)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrollViewReaderBootcamp()
+}
