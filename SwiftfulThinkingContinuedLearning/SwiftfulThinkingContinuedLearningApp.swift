686dfee46e888f7e486fc4a08ceb0a1d78660035
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-10 08:49:00 +0600

    Initial Commit

---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningApp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by jyotirmoy_halder on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulThinkingContinuedLearningTests/SwiftfulThinkingContinuedLearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningTests.swift
+//  SwiftfulThinkingContinuedLearningTests
+//
+//  Created by jyotirmoy_halder on 10/8/25.
+//
+
+import Testing
+@testable import SwiftfulThinkingContinuedLearning
+
+struct SwiftfulThinkingContinuedLearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
