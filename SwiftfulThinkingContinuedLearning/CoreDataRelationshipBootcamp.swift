99434e84b5fe6d816a9d4cb14c0a48b9a0cdc67b
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-14 00:20:18 +0600

    [Feature] Relation between three entity in core data

---
+++ SwiftfulThinkingContinuedLearning/CoreDataRelationshipBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataRelationshipBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by jyotirmoy_halder on 13/8/25.
+//
+
+import SwiftUI
+
+struct CoreDataRelationshipBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataRelationshipBootcamp()
+}
