c4425a9a802dde55892e09dd387f501c4faa9839
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-14 17:30:11 +0600

    [Feature] threadig in swift

---
+++ SwiftfulThinkingContinuedLearning/BackgroundThreadBootcamp.swift
@@ -0,0 +1,66 @@
+//
+//  BackgroundThreadBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by jyotirmoy_halder on 14/8/25.
+//
+
+import SwiftUI
+
+class BackgroundThreadViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    func fetchData() {
+        
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            print("CHECK 1: \(Thread.isMainThread)")
+            print("CHECK 1: \(Thread.current)")
+            
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                print("CHECK 2: \(Thread.isMainThread)")
+                print("CHECK 2: \(Thread.current)")
+            }
+        }
+    }
+    
+    private func downloadData() -> [String] {
+        var data: [String] = []
+        for x in 1...100 {
+            data.append("Item \(x)")
+            print(x)
+        }
+        return data
+    }
+    
+}
+
+struct BackgroundThreadBootcamp: View {
+    
+    @StateObject var vm = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("LOAD DATA")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
+                    }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundStyle(Color.red)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    BackgroundThreadBootcamp()
+}
