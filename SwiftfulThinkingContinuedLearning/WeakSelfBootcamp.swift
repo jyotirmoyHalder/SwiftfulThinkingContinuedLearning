682174a8d85ee51bda33384c910783e6e2054d1e
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-15 15:05:21 +0600

    [Feature]  weak self

---
+++ SwiftfulThinkingContinuedLearning/WeakSelfBootcamp.swift
@@ -0,0 +1,78 @@
+//
+//  WeakSelfBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by jyotirmoy_halder on 14/8/25.
+//
+
+import SwiftUI
+
+struct WeakSelfBootcamp: View {
+    
+    @AppStorage("count") var count: Int?
+    
+    init() {
+        count = 0
+    }
+    
+    var body: some View {
+        NavigationView {
+            NavigationLink("Navigate") {
+                WeakSelfSecondScreen()
+            }
+            .navigationTitle("Screen 1")
+        }
+        .overlay(alignment: .topTrailing) {
+            Text("\(count ?? 0)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.green)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+    }
+}
+
+struct WeakSelfSecondScreen: View {
+    
+    @StateObject var vm = WeakSelfSecondScreenViewModel()
+    
+    var body: some View {
+        Text("Second View")
+            .font(.largeTitle)
+            .foregroundStyle(Color.red)
+        
+        if let data = vm.data {
+            Text("\(data)")
+        }
+    }
+}
+
+class WeakSelfSecondScreenViewModel: ObservableObject {
+    
+    @Published var data: String? = nil
+    
+    init() {
+        print("INITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount + 1, forKey: "count")
+        getData()
+    }
+    
+    deinit {
+        print("DEINITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount - 1, forKey: "count")
+    }
+    
+    func getData() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 500) { [weak self] in
+            self?.data = "NEW DATA!!!"
+        }
+    }
+    
+}
+
+#Preview {
+    WeakSelfBootcamp()
+}
